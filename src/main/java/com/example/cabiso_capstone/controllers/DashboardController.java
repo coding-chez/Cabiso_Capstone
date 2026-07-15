bbeda3cdd5f592ae04f474afdad73d0f284098b0
ches <chestinemaycabiso@gmail.com>
2026-07-15 10:50:52 +0800

    dashboard skeleton

--- src/main/java/com/example/cabiso_capstone/controllers/DashboardController.java
+++ src/main/java/com/example/cabiso_capstone/controllers/DashboardController.java
@@ -1,5 +1,29 @@
 package com.example.cabiso_capstone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class DashboardController {
 
+    public Button dashboardButton;
+
+    public void handleLogout(ActionEvent actionEvent) {
+
+    }
+
+    public void openRoomView(ActionEvent actionEvent) {
+
+    }
+
+    public void showDashboard(ActionEvent actionEvent) {
+
+    }
+
+    public void openTenantView(ActionEvent actionEvent) {
+
+    }
+
+    public void openPaymentView(ActionEvent actionEvent) {
+
+    }
 }
