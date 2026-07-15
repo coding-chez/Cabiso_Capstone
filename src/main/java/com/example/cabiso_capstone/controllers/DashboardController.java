954260294253d0f3f97d106d4479b2c47d4cad01
ches <chestinemaycabiso@gmail.com>
2026-07-15 21:12:34 +0800

    dashboard controller

--- src/main/java/com/example/cabiso_capstone/controllers/DashboardController.java
+++ src/main/java/com/example/cabiso_capstone/controllers/DashboardController.java
@@ -2,17 +2,28 @@ package com.example.cabiso_capstone.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class DashboardController {
 
     public Button dashboardButton;
+    public Label pendingPaymentsLabel;
+    public Label availableRoomsLabel;
+    public Label totalTenantsLabel;
+
+    public void initialize(){
+        //temp values
+        totalTenantsLabel.setText("2");
+        availableRoomsLabel.setText("5");
+        pendingPaymentsLabel.setText("1");
+    }
 
     public void handleLogout(ActionEvent actionEvent) {
-
+        System.out.println("logout"); //checking in 123
     }
 
     public void openRoomView(ActionEvent actionEvent) {
-
+        System.out.println("open room view"); //checking in 123
     }
 
     public void showDashboard(ActionEvent actionEvent) {
@@ -20,10 +31,10 @@ public class DashboardController {
     }
 
     public void openTenantView(ActionEvent actionEvent) {
-
+        System.out.println("tenant view"); //checking in 123
     }
 
     public void openPaymentView(ActionEvent actionEvent) {
-
+        System.out.println("open payment view"); //checking in 123
     }
 }
