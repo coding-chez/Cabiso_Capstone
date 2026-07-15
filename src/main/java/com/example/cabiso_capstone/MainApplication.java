461417df75f610563f2f4b50386e348c9a58af53
ches <chestinemaycabiso@gmail.com>
2026-07-15 21:03:54 +0800

    dashboard UI and styles

--- src/main/java/com/example/cabiso_capstone/MainApplication.java
+++ src/main/java/com/example/cabiso_capstone/MainApplication.java
@@ -10,13 +10,10 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("dashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Dormitory Management System");
         stage.setScene(scene);
-       // stage.setFullScreen(true);
-        stage.setMaxHeight(780);
-        stage.setMaxWidth(1080);
         stage.show();
     }
 }
