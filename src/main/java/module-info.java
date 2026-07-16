2d71df0f688bed8ade96d406b95a4032e14727ad
ches <chestinemaycabiso@gmail.com>
2026-07-16 22:01:28 +0800

    dependencies and requires sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.cabiso_capstone {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.cabiso_capstone to javafx.fxml;
     exports com.example.cabiso_capstone;
@@ -9,7 +9,6 @@ module com.example.cabiso_capstone {
     opens com.example.cabiso_capstone.controllers to javafx.fxml;
     opens com.example.cabiso_capstone.model to javafx.fxml;
     opens com.example.cabiso_capstone.service to javafx.fxml;
-    //opens com.example.cabiso_capstone.data to javafx.fxml;
-    //opens com.example.cabiso_capstone.exception to javafx.fxml;
-    //opens com.example.cabiso_capstone.util to javafx.fxml;
+    opens com.example.cabiso_capstone.database to javafx.fxml;
+//  opens com.example.cabiso_capstone.exception to javafx.fxml;
 }
