f0fd164523868096a24539ed2c9fd9f64af8b0ca
ches <chestinemaycabiso@gmail.com>
2026-07-16 19:52:33 +0800

    room and payment view UIs

--- src/main/java/com/example/cabiso_capstone/controllers/PaymentController.java
+++ src/main/java/com/example/cabiso_capstone/controllers/PaymentController.java
@@ -1,4 +1,57 @@
 package com.example.cabiso_capstone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class PaymentController {
+    public TextField searchField;
+    public ComboBox filterStatusComboBox;
+    public Label recordCountLabel;
+    public TableView paymentTable;
+    public TableColumn paymentIdColumn;
+    public TableColumn tenantNameColumn;
+    public TableColumn roomColumn;
+    public TableColumn amountColumn;
+    public TableColumn paymentDateColumn;
+    public TableColumn paymentMethodColumn;
+    public TableColumn statusColumn;
+    public TextField paymentIdField;
+    public ComboBox tenantComboBox;
+    public TextField amountField;
+    public DatePicker paymentDatePicker;
+    public ComboBox paymentMethodComboBox;
+    public ComboBox statusComboBox;
+    public TextField referenceNumberField;
+
+    public void handleRecordPayment(ActionEvent actionEvent) {
+    }
+
+    public void handleUpdatePayment(ActionEvent actionEvent) {
+    }
+
+    public void handleDeletePayment(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
+
+    public void handleRefresh(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    public void openDashboard(ActionEvent actionEvent) {
+    }
+
+    public void openTenantView(ActionEvent actionEvent) {
+    }
+
+    public void openRoomView(ActionEvent actionEvent) {
+    }
+
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+
 }
--- src/main/java/com/example/cabiso_capstone/controllers/RoomController.java
+++ src/main/java/com/example/cabiso_capstone/controllers/RoomController.java
@@ -1,4 +1,56 @@
 package com.example.cabiso_capstone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class RoomController {
+    public TextField occupantsField;
+    public TextField searchField;
+    public ComboBox filterStatusComboBox;
+    public Label recordCountLabel;
+    public TableView roomTable;
+    public TableColumn roomIdColumn;
+    public TableColumn roomNumberColumn;
+    public TableColumn capacityColumn;
+    public TableColumn occupantsColumn;
+    public TableColumn availableSlotsColumn;
+    public TableColumn monthlyRateColumn;
+    public TableColumn statusColumn;
+    public TextField roomIdField;
+    public TextField roomNumberField;
+    public TextField capacityField;
+    public TextField monthlyRateField;
+    public ComboBox statusComboBox;
+    public Label formMessageLabel;
+
+    public void openTenantView(ActionEvent actionEvent) {
+    }
+
+    public void openDashboard(ActionEvent actionEvent) {
+    }
+
+    public void openPaymentView(ActionEvent actionEvent) {
+    }
+
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    public void handleRefresh(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteRoom(ActionEvent actionEvent) {
+    }
+
+    public void handleUpdateRoom(ActionEvent actionEvent) {
+    }
+
+    public void handleAddRoom(ActionEvent actionEvent) {
+
+    }
 }
