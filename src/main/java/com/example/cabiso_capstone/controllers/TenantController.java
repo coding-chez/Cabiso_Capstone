34e98c46dca5ff43b09732afc61ee91146752650
ches <chestinemaycabiso@gmail.com>
2026-07-16 19:25:37 +0800

    tenant view

--- src/main/java/com/example/cabiso_capstone/controllers/TenantController.java
+++ src/main/java/com/example/cabiso_capstone/controllers/TenantController.java
@@ -1,4 +1,40 @@
 package com.example.cabiso_capstone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class TenantController {
+    public TextField searchField;
+
+    public void handleAddTenant(ActionEvent actionEvent) {
+
+    }
+
+    public void handleUpdateTenant(ActionEvent actionEvent) {
+
+    }
+
+    public void handleDeleteTenant(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
+
+    public void handleRefresh(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    public void openPaymentView(ActionEvent actionEvent) {
+    }
+
+    public void openRoomView(ActionEvent actionEvent) {
+    }
+
+    public void openDashboard(ActionEvent actionEvent) {
+    }
 }
